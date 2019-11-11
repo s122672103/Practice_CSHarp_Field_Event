b5e00f327a7b6eeead9531d1b395385dd153cedc
OLIVER  HSU <s122672103@gmail.com>
2019-11-11 23:27:27 +0800

    欄位、事件練習

---
+++ UnityProject/Assets/Script/chick.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class chick : MonoBehaviour
+{
+    #region 定義
+
+    [Header("移動速度"), Range(1.0f, 100.0f)]
+    public float Speed = 20;
+    [Header("跳躍高度"),Range(1, 2000)]
+    public int Jump = 200;
+    [Header("對話內容")]
+    public string Talk = "咕咕咕~~~";
+    [Header("是否取得雞蛋")]
+    public bool Egg;
+    #endregion
+
+    private void Start()
+    {
+        print("遊戲開始!!!");
+
+    }
+
+    private void Update()
+    {
+        print("咕咕咕~~~");
+    }
+
+}
